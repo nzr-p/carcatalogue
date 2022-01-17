48e7ccd4d13a86351238c1638419ecd9a44883f3
Nazar <1>
2022-01-17 07:55:18 +0300

    Ready

--- install/components/nzrp/carcatalogue.api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
-header("Content-type: application/json; charset=utf-8");
-
-/** @global $APPLICATION */
-$APPLICATION->IncludeComponent(
-	"nzrp:carcatalogue.api",
-	""
-);
--- install/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$arModuleVersion=[
-	"VERSION"     =>"1.0.0",
-	"VERSION_DATE"=>"2022-01-15 20:00:00"
-];
